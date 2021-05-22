708740972ecbf42d89267d2457ad6c19f2853081
su-yash-7 <suyashpradhan7@gmail.com>
2021-05-22 21:39:40 +0530

    MoreButtonFunction

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
 const form = document.querySelector(".search-form");
 let searchValue;
+const more = document.querySelector(".more");
+let page = 1;
+let fetchLink;
+let currentSearch;
 
 //event listiner
 searchInput.addEventListener("input", updateInput);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    currentSearch = searchValue;
     searchPhotos(searchValue)
-})
+
+});
+more.addEventListener("click",loadMore);
+
 function updateInput(e){
     searchValue = e.target.value;
 }
@@ -40,18 +48,30 @@ function genetarePicturs(data){
     });
 }
 async function curatedPhotos(){
-    const data = await fetchApi("https://api.pexels.com/v1/curated?per_page=15&per_page=1")
+    fetchLink = "https://api.pexels.com/v1/curated?per_page=15&per_page=1";
+    const data = await fetchApi(fetchLink)
     genetarePicturs(data);
 }
 async function searchPhotos(query){
     clear();
-    const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`)
+    fetchLink = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`;
+    const data = await fetchApi(fetchLink)
     genetarePicturs(data);
 
 }
 function clear(){
     gallery.innerHTML = "";
     searchInput.value = "";
+}
+async function loadMore(){
+    page++;
+    if(currentSearch){
+        fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}}+query&per_page=15&page=${page}`;
+    }else{
+        fetchLink = `https://api.pexels.com/v1/curated?per_page=15&per_page=${page}`;
+    }
+    const data = await fetchApi(fetchLink)
+    genetarePicturs(data); 
 }
 curatedPhotos(); 
 
